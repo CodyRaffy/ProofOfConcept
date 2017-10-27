f1c91a614804f5d553d8a55da0c7169ebad0b89b
Cody Raffensperger <cody.raffensperger@gmail.com>
2017-10-26 22:35:03 -0400

    Add Sass loader to webpack.  Switch the css file to a scss file with some sass functionality

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,20 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.css$/, loader: "style-loader!css-loader" },
-      { test: /\.js$/, loader: "babel-loader", exclude: /node_modules/, query:{presets:['es2015']} }
+      { test: /\.js$/, loader: "babel-loader", exclude: /node_modules/, query:{presets:['es2015']} },
+      {test: /\.scss$/,
+        use: [
+          {
+             loader: "style-loader" // creates style nodes from JS strings 
+          }, 
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          }, 
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ]
+      }
     ]
   }
 }
