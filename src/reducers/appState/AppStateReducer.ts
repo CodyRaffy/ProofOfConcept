893a5f08f1c9f2784084ad4e75a624b56cc78c41
Cody Raffensperger <cody.raffensperger@gmail.com>
2017-12-12 16:00:57 -0500

    -Fix issue with missing default value

--- src/reducers/appState/AppStateReducer.ts
+++ src/reducers/appState/AppStateReducer.ts
@@ -2,8 +2,12 @@ import { PlayerActionTypes } from "./../../actions/PlayerActions";
 import { ActionTypeKeys } from "./../../actions/ActionTypeKeys";
 import { IAppState } from "../../types/App";
 
+const defaultState : IAppState = {
+    currentGameId: undefined,
+    editPlayerId: undefined
+}
 
-export function appStateReducer(appState: IAppState, action: PlayerActionTypes) {
+export function appStateReducer(appState: IAppState = defaultState, action: PlayerActionTypes) {
     switch (action.type) {
         case ActionTypeKeys.EDIT_PLAYER:
             return { ...appState, editPlayerId: action.id};
